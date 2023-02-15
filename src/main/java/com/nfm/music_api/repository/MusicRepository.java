d5fde60ef923b7b7793692362cbdd700aab0a35c
NikFMatten <nfmatten@gmail.com>
2023-02-15 13:55:55 -0700

    feat: Created custom signatures to repository layer to find music by: name, artist, genre and year

--- src/main/java/com/nfm/music_api/repository/MusicRepository.java
+++ src/main/java/com/nfm/music_api/repository/MusicRepository.java
@@ -3,6 +3,13 @@ package com.nfm.music_api.repository;
 import com.nfm.music_api.data.Music;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MusicRepository extends JpaRepository<Music, Integer> {
-    
+
+    public List<Music> findByName(String name);
+    public List<Music> findByArtist(String artist);
+    public List<Music> findByGenre(String genre);
+    public List<Music> findByYear(Integer year);
+
 }
