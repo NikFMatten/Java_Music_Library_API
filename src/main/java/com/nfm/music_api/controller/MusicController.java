7f1eba952d70fdf5f85e2dd0e4256d340bd7c315
NikFMatten <nfmatten@gmail.com>
2023-02-15 12:59:18 -0700

    feat: Created function in controller layer to return a list of all music from database

--- src/main/java/com/nfm/music_api/controller/MusicController.java
+++ src/main/java/com/nfm/music_api/controller/MusicController.java
@@ -3,9 +3,12 @@ package com.nfm.music_api.controller;
 import com.nfm.music_api.data.Music;
 import com.nfm.music_api.service.MusicService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MusicController {
@@ -16,5 +19,10 @@ public class MusicController {
     @PostMapping("/addMusic")
     public Music addMusic(@RequestBody Music music){
         return musicService.saveMusic(music);
+    }
+
+    @GetMapping("/music")
+    public List<Music> findAllMusic(){
+        return musicService.getAllMusic();
     }
 }
